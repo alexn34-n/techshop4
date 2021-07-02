b0c109c0f45c1106a46d68da7bf4dd21a2ba8c8d
alexn34-n <alexn3469@mail.ru>
2021-07-02 09:08:24 +0300

    Initial commit

---
+++ src/test/java/com/example/techshop4/Techshop4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techshop4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Techshop4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
